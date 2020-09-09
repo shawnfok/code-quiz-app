46a13abba84ff1cf834e32459404ce655902aefd
shawnfok <shawnfok@gmail.com>
2020-09-09 06:05:00 -0400

    adding major files

---
+++ app.js
@@ -0,0 +1,70 @@
+var questionNumber = document.querySelector(".question-number");
+var questionText = document.querySelector(".question-text");
+var optionContainer = document.querySelector(".option-container");
+
+var questionCounter = 0;
+var currentQuestion;
+var availableQuestions = [];
+var availableOptions = [];
+
+// push questions into availableQuestions array
+function setAvailableQuestions() {
+    var totalQuestion = quiz.length;
+    for (var i = 0; i < totalQuestion; i++) {
+        availableQuestions.push(quiz[i])
+    }
+}
+
+// set question number and question and options
+function getNewQuestion() {
+    // set question number
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + "5";
+
+    // set question text
+    // get ramdom question
+    var questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+
+    //  get the position of questionIndex from availableQuestions array
+    var index1 = availableQuestions.indexOf(questionIndex);
+
+    // UNSOLVED : remove the questionIndex from availableQuestions array to prevent repetition
+    availableQuestions.splice(index1, 1);
+
+    // set options
+    // get length of options
+    var optionLength = currentQuestion.options.length;
+
+    // UNSOLVED : push options into availableOptions array
+    for (var i=0; i<optionLength; i++) {
+        availableOptions.push(i)
+    }
+
+    // UNSOLVED : create options in html
+    for (var i=0; i<optionLength; i++) {
+        var option = document.createElement("div");
+        option. innerHTML = currentQuestion.options[i];
+        option.id = i;
+        option.className = "option";
+        optionContainer.appendChild(option)
+    }
+    // add count to counter
+    questionCounter++
+}
+
+
+function next() {
+    if (questionCounter === 5) {
+        console.log("Quiz Over")
+    } else {
+        getNewQuestion();
+    }
+}
+
+window.onload = function () {
+    //first set all questions into availableQuestions array
+    setAvailableQuestions();
+    //then call getNewQuestion() function
+    getNewQuestion()
+}
---
+++ quiz.js
@@ -0,0 +1,63 @@
+// Array of questions
+var quiz = [
+    {
+        q: "What tag is used to render or transform text into an important (bold) version?",
+        options: ["u", "strong", "a", "blockquote"],
+        answer: 1
+    },
+    {
+        q: "What tag is used to define a standard cell inside a table?",
+        options: ["td", "body", "h1 to h6", "footer"],
+        answer: 0
+    },
+    {
+        q: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+        options: ["Restriction", "Output Level", "Range", "Scope"],
+        answer: 3
+    },
+    {
+        q: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+        options: ["Quotations", "Comments", "Comparisons", "Notes"],
+        answer: 1
+    },
+    {
+        q: "External stylesheets are stored in what type of files?",
+        options: ["HTML", "Javascript", "CSS", "None of the above"],
+        answer: 2
+    },
+    {
+        q: "What is the CSS property that sets the size of the whitespace outside the borders of the content?",
+        options: ["Block-level", "Margin", "Spacer", "Padding"],
+        answer: 1
+    },
+    {
+        q: "What is the name of the property that is used to define the special state of an element?",
+        options: ["Pseudo-class", "Syntax", "Style", "Alignment"],
+        answer: 0
+    },
+    {
+        q: "What tag defines a division or the beginning/end of an individual section in an HTML document?",
+        options: ["img", "table", "div", "meta"],
+        answer: 2
+    },
+    {
+        q: "Round the number 4.87 to the nearest integer.",
+        options: ["Floor.math(4.87);", "Round.math(4.87);", "Math.floor(4.87);", "Math.round(4.87);"],
+        answer: 3
+    },
+    {
+        q: "Set the variable hobby to the string 'programming'.",
+        options: ["function hobby ('programming');", "var hobby = 'programming';", "'hobby' === 'programming';", "returns hobby;"],
+        answer: 1
+    },
+    {
+        q: "How do you determine the current Git state of the project?",
+        options: ["$ git clone", "$ git status", "$ git log", "$ git push"],
+        answer: 1
+    },
+    {
+        q: "Print the string 'I just ate a lot' to the console.",
+        options: ["console.log(I just ate a lot);", "log.console(I just ate a lot);", "console.log('I just ate a lot');", "log.console('I just ate a lot');"],
+        answer: 2
+    },
+]
